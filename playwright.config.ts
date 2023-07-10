f6fee1e581a0fb43d513c91ab327c196c38eeda2
jeeshan12 <mohdjeeshan007@gmail.com>
2023-07-10 23:42:16 +0200

    feat: Added monocart report

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,7 +19,13 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: [
+    ['list'],
+    ['monocart-reporter', {
+      name: "My Test Report",
+      outputFile: './test-results/monocart.html'
+    }]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   snapshotPathTemplate:
     "{testDir}/__screenshots__/{platform}/{projectName}/{testFileName}/{arg}{ext}",
