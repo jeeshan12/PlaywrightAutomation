5e69ecac7791c312b665b0a2ace7f5cfd15aa545
jeeshan12 <mohdjeeshan007@gmail.com>
2024-06-02 20:47:02 +0530

    fix(tests): added tags to tests

--- tests/web/login.spec.ts
+++ tests/web/login.spec.ts
@@ -1,8 +1,10 @@
 import { test } from "../../fixtures/order-fixture";
 import { compareSnapshots } from "../../utils/visual-utils";
 test.describe("Snapshot tests for Swag Labs", async () => {
-  test("Visual Test for Swag Store @visual", async ({  page }) => {
-    
-    await compareSnapshots(page, 'swag-store', true);
-  });
+	test("Visual Test for Swag Store @visual", {
+		tag: "@visual"
+	}, async ({ page }) => {
+
+		await compareSnapshots(page, 'swag-store', true);
+	});
 });
