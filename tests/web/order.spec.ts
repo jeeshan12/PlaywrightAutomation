a142b5e18f97ae1f6534aa174684d39328c92eb9
jeeshan12 <mohdjeeshan007@gmail.com>
2024-06-02 20:47:02 +0530

    fix(tests): added tags to tests

--- tests/web/order.spec.ts
+++ tests/web/order.spec.ts
@@ -2,35 +2,37 @@ import { test, expect } from "../../fixtures/order-fixture";
 import { DataFactory } from "../../data/datafactory";
 const ITEM_ONE_INDEX = 0;
 const ITEM_TWO_INDEX = 1;
-test("Place Order", async ({
-  productPage,
-  cartPage,
-  customerInformationPage,
-  overviewPage,
-  completePage,
+test("Place Order", {
+	tag: "@web"
+}, async ({
+	productPage,
+	cartPage,
+	customerInformationPage,
+	overviewPage,
+	completePage,
 }) => {
-  await productPage.clickAddToCartButton(ITEM_ONE_INDEX);
-  await productPage.clickAddToCartButton(ITEM_TWO_INDEX);
-  await expect(productPage.shoppingCartLink).toHaveText("2");
-  await productPage.clickShoppingCartLink();
-  const firstItemCost = await cartPage.cartItemPrice(ITEM_ONE_INDEX).innerText();
-  const secondItemCost = await cartPage.cartItemPrice(ITEM_TWO_INDEX).innerText();
-  await cartPage.clickCheckOutButton();
-  await customerInformationPage.enterInformation(
-    DataFactory.getCustomerInformation()
-  );
-  await customerInformationPage.clickContinueButton();
-  await expect(overviewPage.cartItemPrice(ITEM_ONE_INDEX)).toHaveText(
-    firstItemCost
-  );
-  await expect(overviewPage.cartItemPrice(ITEM_TWO_INDEX)).toHaveText(
-    secondItemCost
-  );
-  await overviewPage.clickFinishButton();
-  await expect(completePage.orderCompleteMessage).toHaveText(
-    "Thank you for your order!"
-  );
-  await expect(completePage.orderCompleteTextMessage).toHaveText(
-    "Your order has been dispatched, and will arrive just as fast as the pony can get there!"
-  );
+	await productPage.clickAddToCartButton(ITEM_ONE_INDEX);
+	await productPage.clickAddToCartButton(ITEM_TWO_INDEX);
+	await expect(productPage.shoppingCartLink).toHaveText("2");
+	await productPage.clickShoppingCartLink();
+	const firstItemCost = await cartPage.cartItemPrice(ITEM_ONE_INDEX).innerText();
+	const secondItemCost = await cartPage.cartItemPrice(ITEM_TWO_INDEX).innerText();
+	await cartPage.clickCheckOutButton();
+	await customerInformationPage.enterInformation(
+		DataFactory.getCustomerInformation()
+	);
+	await customerInformationPage.clickContinueButton();
+	await expect(overviewPage.cartItemPrice(ITEM_ONE_INDEX)).toHaveText(
+		firstItemCost
+	);
+	await expect(overviewPage.cartItemPrice(ITEM_TWO_INDEX)).toHaveText(
+		secondItemCost
+	);
+	await overviewPage.clickFinishButton();
+	await expect(completePage.orderCompleteMessage).toHaveText(
+		"Thank you for your order!"
+	);
+	await expect(completePage.orderCompleteTextMessage).toHaveText(
+		"Your order has been dispatched, and will arrive just as fast as the pony can get there!"
+	);
 });
