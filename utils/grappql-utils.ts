df7c6e64a917a3e8082b0f3b080412ab07d5d58a
jeeshan12 <mohdjeeshan007@gmail.com>
2024-06-02 20:33:43 +0530

    fix(web): updated pw version

--- utils/grappql-utils.ts
+++ utils/grappql-utils.ts
@@ -1,53 +1,53 @@
 import {
-  APIRequestContext,
-  APIResponse,
-  TestInfo,
+	APIRequestContext,
+	APIResponse,
+	TestInfo,
 } from "@playwright/test";
 import { GraphQLOptions } from "../model/graphql-options";
 
 export async function performGraphQLOperation(
-  apiRequestContext: APIRequestContext,
-  graphQLOptions: GraphQLOptions,
-  testInfo: TestInfo
+	apiRequestContext: APIRequestContext,
+	graphQLOptions: GraphQLOptions,
+	testInfo: TestInfo
 ): Promise<APIResponse> {
-  const headers = graphQLOptions?.headers
-    ? graphQLOptions.headers
-    : {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      };
-  const graphQLVariables = graphQLOptions?.queryVariables
-    ? graphQLOptions.queryVariables
-    : {};
+	const headers = graphQLOptions?.headers
+		? graphQLOptions.headers
+		: {
+			"Content-Type": "application/json",
+			Accept: "application/json",
+		};
+	const graphQLVariables = graphQLOptions?.queryVariables
+		? graphQLOptions.queryVariables
+		: {};
 
-  if (graphQLOptions?.attachRequestToReports) {
-    testInfo.annotations.push({
-      type: "GraphQL Query",
-      description: graphQLOptions.query,
-    });
-  }
-  const response = await apiRequestContext.post(graphQLOptions.url, {
-    headers: {
-      ...headers,
-    },
-    data: {
-      query: graphQLOptions.query,
-      variables: {
-        ...graphQLVariables,
-      },
-    },
-  });
-  if (graphQLOptions?.attachResponseToReports) {
-    testInfo.annotations.push(
-      {
-        type: "Status Code",
-        description: String(await response.status()),
-      },
-      {
-        type: "GraphQL Response",
-        description: JSON.stringify(await response.json()),
-      }
-    );
-  }
-  return response;
+	if (graphQLOptions?.attachRequestToReports) {
+		testInfo.annotations.push({
+			type: "GraphQL Query",
+			description: graphQLOptions.query,
+		});
+	}
+	const response = await apiRequestContext.post(graphQLOptions.url, {
+		headers: {
+			...headers,
+		},
+		data: {
+			query: graphQLOptions.query,
+			variables: {
+				...graphQLVariables,
+			},
+		},
+	});
+	if (graphQLOptions?.attachResponseToReports) {
+		testInfo.annotations.push(
+			{
+				type: "Status Code",
+				description: String(await response.status()),
+			},
+			{
+				type: "GraphQL Response",
+				description: JSON.stringify(await response.json()),
+			}
+		);
+	}
+	return response;
 }
--- utils/put-utils.ts
+++ utils/put-utils.ts
@@ -2,61 +2,61 @@ import { APIRequestContext, APIResponse, TestInfo } from "@playwright/test";
 import { PutMethodOptions } from "../model/restapi-options";
 
 export async function performPutOperation(
-  request: APIRequestContext,
-  testInfo: TestInfo,
-  putOptions: PutMethodOptions
+	request: APIRequestContext,
+	testInfo: TestInfo,
+	putOptions: PutMethodOptions
 ) {
-  if (putOptions?.data && putOptions?.form && putOptions?.multipart) {
-    throw new Error(
-      "Please provide data, form or multipart to PUT request body"
-    );
-  }
+	if (putOptions?.data && putOptions?.form && putOptions?.multipart) {
+		throw new Error(
+			"Please provide data, form or multipart to PUT request body"
+		);
+	}
 
-  const headers = putOptions?.headers
-    ? putOptions.headers
-    : {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      };
-  const params = putOptions?.params ? putOptions.params : {};
-  if (putOptions?.attachRequestToReports && putOptions?.data) {
-    testInfo.annotations.push({
-      type: "Request Body",
-      description: putOptions.data,
-    });
-  }
+	const headers = putOptions?.headers
+		? putOptions.headers
+		: {
+			"Content-Type": "application/json",
+			Accept: "application/json",
+		};
+	const params = putOptions?.params ? putOptions.params : {};
+	if (putOptions?.attachRequestToReports && putOptions?.data) {
+		testInfo.annotations.push({
+			type: "Request Body",
+			description: putOptions.data,
+		});
+	}
 
-  let apiContext = {
-    headers: {
-      ...headers,
-    },
-    params: {
-      ...params,
-    },
-  };
-  if (putOptions?.data) {
-    apiContext = { ...apiContext, ...{ data: putOptions.data } };
-  }
-  if (putOptions?.form) {
-    apiContext = { ...apiContext, ...{ form: putOptions.form } };
-  }
-  if (putOptions?.multipart) {
-    apiContext = { ...apiContext, ...{ multipart: putOptions.multipart } };
-  }
-  const response: APIResponse = await request.put(putOptions.url, apiContext);
+	let apiContext = {
+		headers: {
+			...headers,
+		},
+		params: {
+			...params,
+		},
+	};
+	if (putOptions?.data) {
+		apiContext = { ...apiContext, ...{ data: putOptions.data } };
+	}
+	if (putOptions?.form) {
+		apiContext = { ...apiContext, ...{ form: putOptions.form } };
+	}
+	if (putOptions?.multipart) {
+		apiContext = { ...apiContext, ...{ multipart: putOptions.multipart } };
+	}
+	const response: APIResponse = await request.put(putOptions.url, apiContext);
 
-  if (putOptions?.attachResponseToReports) {
-    testInfo.annotations.push(
-      {
-        type: "Status Code",
-        description: String(await response.status()),
-      },
-      {
-        type: "API Response",
-        description: JSON.stringify(await response.json()),
-      }
-    );
-  }
+	if (putOptions?.attachResponseToReports) {
+		testInfo.annotations.push(
+			{
+				type: "Status Code",
+				description: String(await response.status()),
+			},
+			{
+				type: "API Response",
+				description: JSON.stringify(await response.json()),
+			}
+		);
+	}
 
-  return response;
+	return response;
 }
